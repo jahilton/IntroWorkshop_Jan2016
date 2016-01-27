0a232bbb1644daffdc89630ead1697ea934dbe56
jahilton <jahilton@stanford.edu>
2016-01-27 15:00:31 -0800

    Create simple_list.py

---
+++ simple_list.py
@@ -0,0 +1,11 @@
+animals = ['dog','cat','bear','kangaroo']
+
+print(animals[0])
+print(animals[2])
+
+print('the internet is filled with',animals[1],'videos')
+print('the internet is filled with %s videos' % animals[1])
+
+print(animals[1:])
+
+print(len(animals))
