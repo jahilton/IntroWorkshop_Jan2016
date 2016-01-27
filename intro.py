c7566aa5817a87f9dfdf7597babcbec7fd20c660
jahilton <jahilton@stanford.edu>
2016-01-27 14:59:57 -0800

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,8 @@
+year = 2016
+age = 31
+first_name = 'Jason'
+birth_year = year - age
+
+print(first_name,'was born in',birth_year)
+print('my name is %s' % first_name)
+print('It is %d. I am %d.' % (year,age))
